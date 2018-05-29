bcb5ccfad15e8c61cb9a3f7e0a431fe8cd131a69
Filipe Paiva <filipe.p@soltroiavillas.com>
2018-05-29 09:01:52 +0100

    lecture beyond CRUD, complex routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'restaurants/index'
+  end
+  get 'reviews/new'
+  resources :restaurants do 
+  	
+  	collection do 
+  		get 'top', to: 'restaurants#top'
+  	end
+  	member do
+  		get 'chef', to: 'restaurants#chef'
+  	end
+  	resources :reviews, only: [:new, :create]
+
+  end
+
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
+
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        3,
+    chef: "Gordon"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        3,
+    chef: "Other Dude"
+  },
+    {
+    name:         "Filipe's Place",
+    address:      'Lisbon',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        5,
+    chef: "ME :)"
+  },
+  {
+    name:         'Benedict',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4,
+    chef: "Amzing Chef"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
